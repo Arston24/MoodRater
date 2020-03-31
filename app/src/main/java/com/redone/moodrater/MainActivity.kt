1f959668db625f04cec572de4d5d16a99a9f668a
Victor Sysuev <arston_sv@outlook.com>
2020-03-31 16:53:37 +0500

    [chg] some changes

--- app/src/main/java/com/redone/moodrater/MainActivity.kt
+++ app/src/main/java/com/redone/moodrater/MainActivity.kt
@@ -1,169 +1,266 @@
 package com.redone.moodrater
 
+import android.animation.AnimatorSet
+import android.animation.ValueAnimator
 import android.content.Context
+import android.graphics.*
 import android.os.Bundle
 import android.os.Handler
+import android.util.TypedValue
 import android.view.*
-import android.widget.FrameLayout
-import android.widget.PopupWindow
-import android.widget.SeekBar
-import android.widget.TextView
+import android.widget.*
 import androidx.appcompat.app.AppCompatActivity
 import jp.wasabeef.blurry.Blurry
-import kotlinx.android.synthetic.main.popup_view.view.*
 
 
-class MainActivity : AppCompatActivity(), View.OnTouchListener {
+class MainActivity : AppCompatActivity() {
 
-    lateinit var seekBar: SeekBar
+    lateinit var moodSeekBar: SeekBar
     lateinit var floatingMoodText: TextView
-    lateinit var popupMoodText: TextView
+    lateinit var mainMoodText: TextView
+    lateinit var setMoodButton: TextView
+    lateinit var textIntoBar: TextView
+    lateinit var textIntoBar2: TextView
     lateinit var popupWindow: PopupWindow
     lateinit var howMoodLabel: ViewGroup
-    lateinit var moodLabel: ViewGroup
     lateinit var root: ViewGroup
+    lateinit var frameSeek: ViewGroup
+    lateinit var imageView: ImageView
+    lateinit var imageForBlur: ImageView
+    lateinit var titleMood: TextView
+    var startY: Float = 0f
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val chooser = findViewById<View>(R.id.chooser)
-        val mainMoodText = findViewById<TextView>(R.id.mainMoodText)
-        val moodSeekBar = findViewById<SeekBar>(R.id.moodSeekBar)
-        root = findViewById<ViewGroup>(R.id.root)
-        moodLabel = findViewById<ViewGroup>(R.id.moodLabel)
-        howMoodLabel = findViewById<ViewGroup>(R.id.howMoodLabel)
+        mainMoodText = findViewById(R.id.mainMoodText)
+        moodSeekBar = findViewById(R.id.moodSeekBar)
+        root = findViewById(R.id.root)
+        howMoodLabel = findViewById(R.id.howMoodLabel)
+        frameSeek = findViewById(R.id.frameSeek)
+        setMoodButton = findViewById(R.id.setMoodButton)
+        imageView = findViewById(R.id.imageView)
+        textIntoBar = findViewById(R.id.textIntoBar)
+        textIntoBar2 = findViewById(R.id.textIntoBar2)
+        titleMood = findViewById(R.id.titleMood)
+        floatingMoodText = findViewById(R.id.floatingMoodText)
+        imageForBlur = findViewById(R.id.imageForBlur)
 
-        val inflater = getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
-
-        val popupView = inflater.inflate(R.layout.popup_view, null)
-        seekBar = popupView.seekBar
-        floatingMoodText = popupView.floatingMoodText
-
-        val moodRater = popupView.seekBar
-
-        popupView.setOnTouchListener(this)
-
-
-        popupWindow = PopupWindow(
-            popupView,
-            FrameLayout.LayoutParams.MATCH_PARENT,
-            FrameLayout.LayoutParams.MATCH_PARENT
-        )
-        popupWindow.isOutsideTouchable = true
-        popupWindow.isClippingEnabled = true
-        popupWindow.isFocusable = true
-        popupWindow.inputMethodMode = PopupWindow.INPUT_METHOD_NOT_NEEDED
-        popupWindow.softInputMode = WindowManager.LayoutParams.SOFT_INPUT_STATE_UNSPECIFIED
-        popupWindow.contentView.isFocusableInTouchMode = true
+        howMoodLabel.post {
+            startY = howMoodLabel.y
+        }
 
 
-        popupMoodText = popupView.popupMoodText
-
-        moodRater.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
+        if (moodSeekBar.progress == 0) {
+            moodSeekBar.thumb.alpha = 0
+        }
+        moodSeekBar.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
             override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
-                moodSeekBar.progress = progress
                 if (progress < 20) {
                     mainMoodText.text = getString(R.string.bad_mood)
-                    popupMoodText.text = getString(R.string.bad_mood)
                 } else if (progress in 20..39) {
                     mainMoodText.text = getString(R.string.not_bad_mood)
-                    popupMoodText.text = getString(R.string.not_bad_mood)
                 } else if (progress in 40..59) {
                     mainMoodText.text = getString(R.string.nice_mood)
-                    popupMoodText.text = getString(R.string.nice_mood)
                 } else if (progress in 60..79) {
                     mainMoodText.text = getString(R.string.good_mood)
-                    popupMoodText.text = getString(R.string.good_mood)
                 } else {
                     mainMoodText.text = getString(R.string.awesome_mood)
-                    popupMoodText.text = getString(R.string.awesome_mood)
                 }
             }
 
             override fun onStartTrackingTouch(seekBar: SeekBar?) {
-
+                if (moodSeekBar.thumb.alpha == 0) {
+                    moodSeekBar.thumb.alpha = 255
+                    mainMoodText.visibility = View.VISIBLE
+                    imageForBlur.visibility = View.VISIBLE
+                    val chip = findViewById<ViewGroup>(R.id.chip)
+                    Blurry.with(this@MainActivity)
+                        .radius(10)
+                        .sampling(4)
+                        .async()
+                        .animate(500)
+                        .capture(chip)
+                        .into(imageForBlur)
+                    val parentCenterY: Float = root.y + root.height / 2 - howMoodLabel.height / 2
+                    howMoodLabel.animate().y(parentCenterY).duration = 500
+                    titleMood.text = getString(R.string.how_mood)
+                    root.setOnTouchListener(rootTouchListener())
+                    setMoodButton.visibility = View.VISIBLE
+                }
             }
 
             override fun onStopTrackingTouch(seekBar: SeekBar?) {
 
             }
-
         })
+        setMoodButton.setOnClickListener {
+            setMood()
+        }
 
-        chooser.setOnClickListener {
-            Blurry.with(this)
+        imageView.setOnClickListener {
+            setMoodButton.visibility = View.VISIBLE
+            root.setOnTouchListener(rootTouchListener())
+            titleMood.text = getString(R.string.how_mood)
+            imageForBlur.visibility = View.VISIBLE
+            val chip = findViewById<ViewGroup>(R.id.chip)
+            Blurry.with(this@MainActivity)
                 .radius(10)
                 .sampling(8)
                 .async()
-                .animate(500)
-                .onto(root)
-            popupWindow.animationStyle = R.style.Animation
-            popupWindow.showAtLocation(root, 0, 0, 0)
-        }
+                .animate(1000)
+                .capture(chip)
+                .into(imageForBlur)
+            val parentCenterY: Float = root.y + root.height / 2 - howMoodLabel.height / 2
+            val parentCenterX: Float = root.x + root.width / 2
+            howMoodLabel.animate().y(parentCenterY).duration = 500
 
-        popupView.setMoodButton.setOnClickListener {
-            moodLabel.visibility = View.VISIBLE
-            howMoodLabel.visibility = View.GONE
-            popupWindow.dismiss()
+            val translateAnimator =
+                ValueAnimator.ofFloat(titleMood.x, parentCenterX - mainMoodText.width * 0.37f)
+            translateAnimator.duration = 500
+            translateAnimator.addUpdateListener {
+                val value = translateAnimator.animatedValue as Float
+                mainMoodText.x = value
+            }
+            val sizeAnimator = ValueAnimator.ofFloat(22f, 16f)
+            sizeAnimator.duration = 500
+            sizeAnimator.addUpdateListener {
+                val value = sizeAnimator.animatedValue as Float
+                mainMoodText.textSize = value
+            }
+            val set = AnimatorSet()
+            set.playTogether(translateAnimator, sizeAnimator)
+            set.start()
+
+            val dip = 8f
+            val px = TypedValue.applyDimension(
+                TypedValue.COMPLEX_UNIT_DIP,
+                dip,
+                resources.displayMetrics
+            )
+
+            imageView.animate().scaleX(1f).scaleY(1f).y(moodSeekBar.y + moodSeekBar.height)
+                .x(moodSeekBar.x - px).duration = 500
             Handler().postDelayed({
-                Blurry.delete(root)
+                imageView.visibility = View.GONE
+                moodSeekBar.visibility = View.VISIBLE
+                textIntoBar2.visibility = View.VISIBLE
+                textIntoBar.visibility = View.VISIBLE
             }, 500)
+
         }
     }
 
-    override fun onTouch(v: View?, event: MotionEvent?): Boolean {
-        val seekBarPosition = IntArray(2)
-        seekBar.getLocationOnScreen(seekBarPosition)
-        val seekBarWidth = seekBar.width
-        val bias = seekBarWidth / 100
-        val x = event?.x?.toInt() ?: 0
-        val y = event?.y?.toInt() ?: 0
-        when (event?.action) {
-            MotionEvent.ACTION_DOWN -> {
-                floatingMoodText.visibility = View.VISIBLE
-                val progress = when {
-                    x < seekBarPosition[0] -> {
-                        0
-                    }
-                    x > (seekBarPosition[0] + seekBarWidth) -> {
-                        100
-                    }
-                    else -> {
-                        (x - seekBarPosition[0]) / bias
-                    }
+    fun setMood() {
+        titleMood.text = getString(R.string.mood_title)
+        imageForBlur.visibility = View.GONE
+        frameSeek.post {
+            val bitmapSeekBar = getViewBitmap(frameSeek)
+            imageView.visibility = View.GONE
+            imageView.setImageBitmap(bitmapSeekBar)
+            imageView.visibility = View.VISIBLE
+            moodSeekBar.visibility = View.GONE
+            textIntoBar2.visibility = View.GONE
+            textIntoBar.visibility = View.GONE
+            imageView.post {
+                howMoodLabel.animate().y(startY).duration = 500
+
+                val translateAnimator = ValueAnimator.ofFloat(mainMoodText.x, titleMood.x)
+                translateAnimator.duration = 500
+                translateAnimator.addUpdateListener {
+                    val value = translateAnimator.animatedValue as Float
+                    mainMoodText.x = value
                 }
-                seekBar.progress = progress
-                floatingMoodText.text = popupMoodText.text
-                floatingMoodText.x = x.toFloat()
-                floatingMoodText.y = y.toFloat() - 200f
-            }
-            MotionEvent.ACTION_MOVE -> {
-                val progress = when {
-                    x < seekBarPosition[0] -> {
-                        0
-                    }
-                    x > (seekBarPosition[0] + seekBarWidth) -> {
-                        100
-                    }
-                    else -> {
-                        (x - seekBarPosition[0]) / bias
-                    }
+
+                val sizeAnimator = ValueAnimator.ofFloat(16f, 22f)
+                sizeAnimator.duration = 500
+                sizeAnimator.addUpdateListener {
+                    val value = sizeAnimator.animatedValue as Float
+                    mainMoodText.textSize = value
                 }
-                seekBar.progress = progress
-                floatingMoodText.text = popupMoodText.text
-                floatingMoodText.x = x.toFloat()
-                floatingMoodText.y = y.toFloat() - 200f
-            }
-            MotionEvent.ACTION_UP -> {
-                moodLabel.visibility = View.VISIBLE
-                howMoodLabel.visibility = View.GONE
-                floatingMoodText.visibility = View.GONE
-                popupWindow.dismiss()
-                Handler().postDelayed({
-                    Blurry.delete(root)
-                }, 500)
+                val set = AnimatorSet()
+                set.playTogether(translateAnimator, sizeAnimator)
+                set.start()
+                imageView.animate().scaleX(0.5f).scaleY(0.5f)
+                    .y(mainMoodText.y - imageView.height / 4)
+                    .x(root.width.toFloat() / 4).duration =
+                    500
+                root.setOnTouchListener(null)
+                setMoodButton.visibility = View.GONE
             }
         }
-        return true
+    }
+
+    fun rootTouchListener(): View.OnTouchListener {
+        return View.OnTouchListener { v, event ->
+            root.performClick()
+            val seekBarPosition = IntArray(2)
+            frameSeek.getLocationOnScreen(seekBarPosition)
+            val seekBarWidth = moodSeekBar.width
+            val bias = seekBarWidth / 100
+            val x = event?.x?.toInt() ?: 0
+            val y = event?.y?.toInt() ?: 0
+            when (event?.action) {
+                MotionEvent.ACTION_DOWN -> {
+                    floatingMoodText.visibility = View.VISIBLE
+                    val progress = when {
+                        x < seekBarPosition[0] -> {
+                            0
+                        }
+                        x > (seekBarPosition[0] + seekBarWidth) -> {
+                            100
+                        }
+                        else -> {
+                            (x - seekBarPosition[0]) / bias
+                        }
+                    }
+                    moodSeekBar.progress = progress
+                    floatingMoodText.text = mainMoodText.text
+                    floatingMoodText.x = x.toFloat()
+                    floatingMoodText.y = y.toFloat() - 200f
+                }
+                MotionEvent.ACTION_MOVE -> {
+                    val progress = when {
+                        x < seekBarPosition[0] -> {
+                            0
+                        }
+                        x > (seekBarPosition[0] + seekBarWidth) -> {
+                            100
+                        }
+                        else -> {
+                            (x - seekBarPosition[0]) / bias
+                        }
+                    }
+                    moodSeekBar.progress = progress
+                    floatingMoodText.text = mainMoodText.text
+                    floatingMoodText.x = x.toFloat()
+                    floatingMoodText.y = y.toFloat() - 200f
+                }
+                MotionEvent.ACTION_UP -> {
+                    floatingMoodText.visibility = View.GONE
+                    setMood()
+                    root.setOnTouchListener(null)
+                    setMoodButton.visibility = View.GONE
+                }
+            }
+            true
+        }
+    }
+
+    fun getViewBitmap(view: View): Bitmap {
+        val returnedBitmap = Bitmap.createBitmap(view.width, view.height, Bitmap.Config.ARGB_8888)
+        val canvas = Canvas(returnedBitmap)
+        val paint = Paint()
+        paint.xfermode = PorterDuffXfermode(PorterDuff.Mode.CLEAR)
+
+        val bgDrawable = view.background
+        if (bgDrawable != null) {
+            bgDrawable.draw(canvas)
+        } else {
+            canvas.drawColor(Color.TRANSPARENT)
+        }
+        view.draw(canvas)
+
+        return returnedBitmap
     }
 }
